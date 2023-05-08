6858361a90cdf97e553747b0a26cb5adcb1bdaf4
franciszekstodulski <franek.stodulski@gmail.com>
2023-05-08 12:44:46 +0200

    🎨 style(StreamProviders.svelte): add price and presentation_type to stream provider list items

--- src/routes/(app)/discover/movie/[id]/components/StreamProviders/StreamProviders.svelte
+++ src/routes/(app)/discover/movie/[id]/components/StreamProviders/StreamProviders.svelte
@@ -34,7 +34,7 @@
 
 <div class="flex flex-col gap-2">
   {#each keys as key, index}
-    <div class="flex w-full px-4">
+    <div class="flex w-full px-4 overflow-x-auto scrollbar-hide">
       <div
         class="provider-label flex justify-center py-3 px-1 rounded-md {index === 0
           ? 'bg-background-dark-strong-alpha/[0.32]'
@@ -44,11 +44,22 @@
       </div>
       <ul class="flex pl-4">
         {#each providers[key] as stream}
-          <li class="flex px-4 pt-2 pb-4 border-b border-border-default-muted-alpha/[0.32]">
+          <li
+            class="flex px-4 pt-2 pb-2 border-b border-border-default-muted-alpha/[0.32] justify-center items-center flex-col gap-1"
+          >
             <div class="w-12 h-12 rounded-md overflow-hidden">
               <a href={stream.provider_url} target="_blank" rel="noreferrer">
                 <img src={parsePoster(stream.image_url, LOGO_SIZES.enum.w45)} alt="" />
               </a>
+            </div>
+            <div class="flex gap-1">
+              <span class="text-t100 text-text-light-strong">
+                {stream.price}
+              </span>
+
+              <span class="text-t100 text-text-action-caution">
+                {stream.presentation_type}
+              </span>
             </div>
           </li>
         {/each}
