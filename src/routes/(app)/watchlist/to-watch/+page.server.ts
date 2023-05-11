d62bbe6b0f95b63e127faf23f91b4b2854e5cca4
franciszekstodulski <franek.stodulski@gmail.com>
2023-05-11 16:40:12 +0200

    🐛 fix(to-watch): add missing await statement to event.parent() call in load function

--- src/routes/(app)/watchlist/to-watch/+page.server.ts
+++ src/routes/(app)/watchlist/to-watch/+page.server.ts
@@ -3,6 +3,7 @@ import { WatchlistRepository } from '$lib/core/repositories/watchlist.repository
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
+  await event.parent();
   const session = await event.locals.getSession();
 
   if (!session) return;
