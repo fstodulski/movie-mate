f428fb323e477a5d4cea230f4dde896382d8b94a
franciszekstodulski <franek.stodulski@gmail.com>
2023-06-02 12:28:50 +0200

    🎨 style(FeedCategoryList.svelte): add margin bottom to the container div ✨ feat(FeedCategoryList.svelte): add movie details to each movie in the category list 🐛 fix(FeedCategoryList.svelte): fix watch on text alignment 🚀 chore(FeedCategoryList.svelte): add bookmark icon to add to watchlist button
    🎨 style(FeedNavigation.svelte): add active class to selected link and adjust styles accordingly

--- src/lib/containers/Feed/components/FeedNavigation.svelte
+++ src/lib/containers/Feed/components/FeedNavigation.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import { Icon } from '@steeze-ui/svelte-icon';
-  import { includes } from 'ramda';
 
   import { LINKS } from './feed.const';
+
+  let selectedLink = null;
 
   const handleAnchorClick = (event) => {
     event.preventDefault();
@@ -11,27 +12,25 @@
     const anchorId = new URL(link.href).hash.replace('#', '');
     const anchor = document.getElementById(anchorId);
 
+    selectedLink = anchorId;
+
     window.scrollTo({
-      top: anchor.offsetTop - 110,
+      top: anchor.offsetTop - 120,
       behavior: 'smooth'
     });
   };
+
+  $: {
+    selectedLink = $page.url.hash.replace('#', '');
+  }
 </script>
 
-<div class="flex items-center overflow-x-auto gap-2 mb-4 scrollbar-hide py-2">
+<div class="flex items-center overflow-x-auto gap-2 scrollbar-hide py-2">
   {#each LINKS as link}
-    <a
-      href={link.href}
-      class="badge flex items-center bg-background-dark-default-alpha/[0.2] border-border-default-muted-alpha/[0.32]"
-      on:click={handleAnchorClick}
-    >
+    {@const active = link.href.includes(selectedLink)}
+    <a href={link.href} class:active class="badge flex items-center" on:click={handleAnchorClick}>
       <div class="flex items-center gap-2">
-        <Icon
-          src={link.icon}
-          size="20px"
-          class={link.css}
-          theme={includes($page.url.hash, link.href) ? 'solid' : ''}
-        />
+        <Icon src={link.icon} size="20px" class={link.css} theme={active ? 'solid' : ''} />
         <span class="whitespace-nowrap text-text-light-strong text-t200">
           {link.label}
         </span>
@@ -42,6 +41,10 @@
 
 <style lang="scss">
   .badge {
-    @apply flex items-center flex-1 h-[40px] px-3  border rounded-md w-full flex items-center last-of-type:mr-4;
+    @apply flex items-center flex-1 h-[40px] px-3  rounded-md w-full last-of-type:mr-4;
+  }
+
+  .active {
+    @apply border bg-background-dark-default-alpha/[0.2] border-border-default-muted-alpha/[0.32] #{!important};
   }
 </style>
